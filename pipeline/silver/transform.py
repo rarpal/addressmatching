ff59d8073f3a31a712a736f8a9821ec456ae600d
Ravi Palihena <rarpal@gmail.com>
2024-03-16 04:19:47 +0000

    load to gold with delta lake

--- pipeline/silver/transform.py
+++ pipeline/silver/transform.py
@@ -1,25 +1,5 @@
 from pyspark.sql import SparkSession, functions as F
 from pyspark.sql import DataFrame, types as T
-
-def read_csv(sps: SparkSession, csvpath: str) -> DataFrame:
-    
-    df_csv = sps.read.csv(csvpath, header=True, inferSchema=True)
-    
-    return df_csv
-
-def read_parquet(sps: SparkSession, parquetpath: str) -> DataFrame:
-    
-    df_parquet = sps.read.parquet(parquetpath)
-    
-    return df_parquet
-
-def write__repart_parquet(sps: SparkSession, df_parquet: DataFrame, numparts: int, parquetpath: str):
-    
-    df_parquet.repartition(numparts).write.mode('overwrite').parquet(parquetpath)
-
-def write_parquet(sps: SparkSession, df_parquet: DataFrame, parquetpath: str):
-
-    df_parquet.write.mode('overwrite').parquet(parquetpath)
     
 def prep_abmatchdframe(sps: SparkSession, df_ab: DataFrame) -> DataFrame:
     
